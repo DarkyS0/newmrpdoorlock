0182e55ef6434db2857b39f494a6e403643a07dc
Ömer <76553634+DarkyS0@users.noreply.github.com>
2021-05-01 22:04:46 +0300

    Add files via upload

---
+++ newmrpdoorlock/newmrpddoors.lua
@@ -0,0 +1,245 @@
+
+{
+         textCoords = vector3(434.4144, -981.8556, 30.6953),
+         authorizedJobs = {'police'},
+         locked = true,
+         distance = 2,
+         doors = {
+             {
+                 objName = "gabz_mrpd_reception_entrancedoor",
+                 objYaw = 270.0,
+                 objCoords = vector3(434.7444, -980.7556, 30.8153)
+             },
+             {
+                 objName = "gabz_mrpd_reception_entrancedoor",
+                 objYaw = 90.0,
+                 objCoords = vector3(434.7444,-983.0781,30.8153)
+            }
+         }
+ },
+ {
+    textCoords = vector3(457.00, -972.28, 30.7153),
+    authorizedJobs = {'police'},
+    locked = true,
+    distance = 2,
+    doors = {
+        {
+            objName = "gabz_mrpd_reception_entrancedoor",
+            objYaw = 180.0,
+            objCoords = vector3(455.8862, -972.2543, 30.81531)
+        },
+        {
+            objName = "gabz_mrpd_reception_entrancedoor",
+            objYaw = 360.0,
+            objCoords = vector3(455.2087, -972.2543, 30.81531)
+       }
+    }
+},
+ {
+    textCoords = vector3(441.8, -998.73, 30.7353),
+    authorizedJobs = {'police'},
+    locked = true,
+    distance = 2,
+    doors = {
+        {
+            objName = "gabz_mrpd_reception_entrancedoor",
+            objYaw = 360.0,
+            objCoords = vector3(440.7392, -998.7462, 30.8153)
+        },
+        {
+            objName = "gabz_mrpd_reception_entrancedoor",
+            objYaw = 180.0,
+            objCoords = vector3(443.0618,-998.7462,30.8153)
+       }
+    }
+},
+{
+textCoords = vector3(469.43, -986.26, 30.79),
+authorizedJobs = {'police'},
+locked = true,
+distance = 2,
+doors = {
+    {
+        objName = "gabz_mrpd_door_01",
+        objYaw = 270.0,
+        objCoords = vector3(469.4406, -985.0313, 30.82319)
+    },
+    {
+        objName = "gabz_mrpd_door_01",
+        objYaw = 90.0,
+        objCoords = vector3(469.4406,-987.4377,30.82319)
+   }
+}
+},
+{
+textCoords = vector3(468.37, -1014.21, 26.42),
+authorizedJobs = {'police'},
+locked = true,
+distance = 2,
+doors = {
+    {
+        objName = "gabz_mrpd_door_03",
+        objYaw = 180.0,
+        objCoords = vector3(469.7743, -1014.406, 26.48697)
+    },
+    {
+        objName = "gabz_mrpd_door_03",
+        objYaw = 360.0,
+        objCoords = vector3(467.3686,-1014.406,26.48697)
+   }
+}
+},
+{
+textCoords = vector3(471.34, -1009.17, 26.37),
+authorizedJobs = {'police'},
+locked = true,
+distance = 2,
+doors = {
+    {
+        objName = "gabz_mrpd_door_02",
+        objYaw = 270.0,
+        objCoords = vector3(471.3679, -1007.793, 26.40548)
+    },
+    {
+        objName = "gabz_mrpd_door_02",
+        objYaw = 90.0,
+        objCoords = vector3(471.3758,-1010.198,26.40548)
+   }
+}
+},
+{
+    objName = 'gabz_mrpd_door_04',
+    objYaw = 360.0,
+    objCoords  = vector3(440.5201, -977.6011, 30.82),
+    textCoords = vector3(441.6701, -977.6011, 30.69), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_door_05',
+    objYaw = 180.0,
+    objCoords  = vector3(440.5201, -986.2335, 30.82319),
+    textCoords = vector3(441.64, -985.96, 30.69), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_door_05',
+    objYaw = 270.0,
+    objCoords  = vector3(458.6543, -990.6498, 30.82),
+    textCoords = vector3(458.79, -989.36, 30.69), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 270.0,
+    objCoords  = vector3(476.6157, -1008.875, 26.48005),
+    textCoords = vector3(476.76, -1008.24, 26.27), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 0.0,
+    objCoords  = vector3(477.9126, -1012.189, 26.48005),
+    textCoords = vector3(477.9126, -1012.189, 26.48005), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+    {
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 0.0,
+    objCoords  = vector3(480.9128, -1012.189, 26.48005),
+    textCoords = vector3(480.9128, -1012.189, 26.48005), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 0.0,
+    objCoords  = vector3(483.9127, -1012.189, 26.48005),
+    textCoords = vector3(483.9127, -1012.189, 26.48005), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 0.0,
+    objCoords  = vector3(486.9131, -1012.189, 26.480005),
+    textCoords = vector3(486.9131, -1012.189, 26.480005), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 180.0,
+    objCoords  = vector3(484.1764, -1007.734, 26.480005),
+    textCoords = vector3(484.1764, -1007.734, 26.480005), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_cells_door',
+    objYaw = 180.0,
+    objCoords  = vector3(481.0084, -1004.118, 26.480005),
+    textCoords = vector3(481.0084, -1004.118, 26.480005), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_door_04',
+    objYaw = 270.0,
+    objCoords  = vector3(482.6703, -995.7285, 26.40548),
+    textCoords = vector3(482.6703, -995.7285, 26.40548), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_door_04',
+    objYaw = 270.0,
+    objCoords  = vector3(482.6699, -992.2991, 26.40548),
+    textCoords = vector3(482.6699, -992.2991, 26.40548), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_door_04',
+    objYaw = 270.0,
+    objCoords  = vector3(482.67010, -987.5792, 26.40548),
+    textCoords = vector3(482.67010, -987.5792, 26.40548), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_door_04',
+    objYaw = 270.0,
+    objCoords  = vector3(482.66940, -983.9868, 26.40548),
+    textCoords = vector3(482.66940, -983.9868, 26.40548), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
+{
+    objName = 'gabz_mrpd_door_03',
+    objYaw = 133.0,
+    objCoords  = vector3(475.8323, -990.4839, 26.40548),
+    textCoords = vector3(475.8323, -990.4839, 26.40548), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_room13_parkingdoor',
+    objYaw = 270.0,
+    objCoords  = vector3(464.1591, -974.6656, 26.3707),
+    textCoords = vector3(464.1591, -974.6656, 26.3707), 
+    authorizedJobs = { 'police' },
+    locked = true
+}, 
+{
+    objName = 'gabz_mrpd_room13_parkingdoor',
+    objYaw = 90.0,
+    objCoords  = vector3(464.1566, -997.5093, 26.3707),
+    textCoords = vector3(464.1566, -997.5093, 26.3707), 
+    authorizedJobs = { 'police' },
+    locked = true
+},
